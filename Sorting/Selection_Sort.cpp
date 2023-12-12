c93f9077c1c15c05b0eb38421e87429884598710
Anirudh Saksena <119091132+A-Knee09@users.noreply.github.com>
2023-12-12 23:36:53 +0530

    Update Selection_Sort.cpp

--- Sorting/Selection_Sort.cpp
+++ Sorting/Selection_Sort.cpp
@@ -1,52 +1,43 @@
-//Written by Anirudh Saksena
 #include <iostream>
 using namespace std;
 
-class sort
+class array
 {
-private:
-int i;
-int j;
-int min_idx;
-
 public:
-// Function for taking input in array
-    void Inp(int arr[], int n)
+    void Inp(int *arr, int n)
     {
-        for (i = 0; i < n; i++)
+        for (int i = 0; i < n; i++)
         {
             cin >> arr[i];
         }
+        cout << endl;
     }
-// Function for displaying array output
-    void Out(int arr[], int n)
+    void Out(int *arr, int n)
     {
-        for (i = 0; i < n; i++)
+        for (int i = 0; i < n; i++)
         {
             cout << arr[i] << " ";
         }
+        cout << endl;
     }
-// Function for Selection Sort
-    void Selec_Sort(int arr[], int n)
+};
+
+class Bubble
+{
+public:
+    int i, j, temp;
+    void sort(int *arr, int n)
     {
         for (i = 0; i < n - 1; i++)
         {
-            // assuming initial postion is the smallest
-            min_idx = i;
-            for (j = i + 1; j < n; j++)
+            for (j = 0; j < n - i - 1; j++)
             {
-                if (arr[min_idx] > arr[j])
+                if (arr[j] > arr[j + 1])
                 {
-                    //assign new smallest position if comparision is true
-                    min_idx = j;
+                    temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
                 }
-            }
-            // swap elements 
-            if (min_idx != i)
-            {
-                int temp = arr[min_idx];
-                arr[min_idx] = arr[i];
-                arr[i] = temp;
             }
         }
     }
@@ -54,16 +45,16 @@ public:
 
 int main()
 {
-    sort s;
-    int arr[100];
+    array *a = new array;
+    Bubble *s = new Bubble;
     int n;
-    cout<<"Enter the size of the array: ";
-    cin>>n;
-    cout << "Enter the array: ";
-    s.Inp(arr , n);
-    s.Out(arr,n);
-    cout<<endl;
-    cout << "The sorted array is: ";
-    s.Selec_Sort(arr, n);
-    s.Out(arr , n);
+    cout << "Enter the size of the array: ";
+    cin >> n;
+    int *arr = new int[n];
+    a->Inp(arr, n);
+    cout << "Orignal array: ";
+    a->Out(arr, n);
+    cout << "Sorted array: ";
+    s->sort(arr, n);
+    a->Out(arr, n);
 }
